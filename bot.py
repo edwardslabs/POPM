8c023798136ef9f5b6630d3174f4c33603da20cf
Matt Ullman <blindsight@gamesurge.net>
2015-01-14 22:58:04 -0500

    Add in socks monitoring

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import time
 import re
 import datetime
 import dns.resolver
+import httplib
 from threading import Thread
 from thread import start_new_thread, allocate_lock
 from config import *
@@ -157,12 +158,42 @@ def http_connect(ip):
             pass
 
         if str(contrue) == "0" or contrue is None:
-            print "Moving on to socks!"
             sockscheck(ip)
 
 def sockscheck(ip):
     if ENABLE_SOCKS != 0:
+        global num_threads, thread_started, contrue
         ports = [1080,1075,10000,10080,10099,10130,10242,10777,1025,1026,1027,1028,1029,1030,1031,1032,1033,1039,1050,1066,1081,1098,11011,11022,11033,11055,11171,1122,11225,1180,1182,1200,1202,1212,1234,12654,1337,14841,16591,17327,1813,18888,1978,1979,19991,2000,21421,22277,2280,24971,24973,25552,25839,26905,28882,29992,3127,3128,32167,3330,3380,34610,3801,3867,40,4044,41080,41379,43073,43341,443,44548,4471,43371,44765,4914,49699,5353,559,58,6000,62385,63808,6551,6561,6664,6748,6969,7007,7080,8002,8009,8020,8080,8085,8111,8278,8751,8888,9090,9100,9988,9999,59175,5001,19794]
+
+        def check_socks(ip, port):
+            global num_threads, thread_started, contrue
+            lock.acquire()
+            num_threads += 1
+            thread_started = True
+            lock.release()
+
+            check=httplib.HTTPConnection('google.com', port, ip, timeout=1)
+            try:
+                check.connect()
+                if check != False:
+                    s.send("%s GL * +*@%s 259200 %d %d :AUTO Using or hosting open proxies is not permitted on %s. [Detected socks/%s]\n" % (SRVID, ip, int(time.time()), int(time.time()) + 259200, NETWORK_NAME, port))
+                    print("[WRITE][SOCKS]: %s GL * +*@%s 259200 %d %d :AUTO Using or hosting open proxies is not permitted on %s. [Detected socks/%s]" % (SRVID, ip, int(time.time()), int(time.time()) + 259200, NETWORK_NAME, port))
+                    contrue += 1
+                    check.close()
+            except socket.error, v:
+                pass
+
+            lock.acquire()
+            num_threads -= 1
+            lock.release()
+
+        for newport in ports:
+            start_new_thread(check_socks, (ip, newport, ))
+
+        while not thread_started:
+            pass
+        while num_threads > 0:
+            pass
 
 signal.signal(signal.SIGINT, signal_handler)
 s=socket.socket()
@@ -175,7 +206,7 @@ s.send("SERVER %s %s %d %d J10 %s]]] :%s\n" % (SRVNAME, HOPS, boot_time, boot_ti
 print("[WRITE]: SERVER %s %s %d %d J10 %s]]] :%s" % (SRVNAME, HOPS, boot_time, boot_time, SRVID, SRVDESC))
 s.send("%s N %s 1 %d %s services.gamesurge.net +oik AAAAAA %sAAA :Proxy Monitor Service\n" % (SRVID, BOT_NAME, boot_time, BOT_NAME, SRVID))
 print("[WRITE]: %s N %s 1 %d %s services.gamesurge.net +oik AAAAAA %sAAA :Proxy Monitor Service" % (SRVID, BOT_NAME, boot_time, SRVID, BOT_NAME))
-s.send("%s B %s %d %sAAA:o\n" % (SRVID, int(time.time()), DEBUG_CHANNEL, SRVID))
+s.send("%s B %s %d %sAAA:o\n" % (SRVID, DEBUG_CHANNEL, int(time.time()), SRVID))
 print("[WRITE]: %s B %s %d %sAAA:o" % (SRVID, DEBUG_CHANNEL, int(time.time()), SRVID))
 s.send("%sAAA M %s +o %sAAA %d\n" % (SRVID, DEBUG_CHANNEL, SRVID, int(time.time())))
 print("[WRITE]: %sAAA M %s +o %sAAA %d" % (SRVID, DEBUG_CHANNEL, SRVID, int(time.time())))
@@ -269,5 +300,5 @@ while 1:
                     print("[WRITE]: %sAAA P %s :There are no threads running" % (SRVID, line[2]))
             elif(line[3] == ":.help"):
                 s.send("%sAAA O %s :-=-=-=-=-=-= %s Help -=-=-=-=-=-=\n" % (SRVID, line[0], BOT_NAME))
-                s.send("%sAAA O %s :No information available\n" % (SRVID, line[0]))
+                s.send("%sAAA O %s :No information available at this time.\n" % (SRVID, line[0]))
                 s.send("%sAAA O %s :-=-=-=-=-=-= End Of Help -=-=-=-=-=-=\n" % (SRVID, line[0]))
