0599c58e085153e25e7bf740fa08020cf48d4656
Matt Ullman <blindsight@gamesurge.net>
2015-01-20 23:40:29 -0500

    New socks function test

--- proxy.py
+++ proxy.py
@@ -130,29 +130,72 @@ def http_connect(ip, HTTPTRUE, SOCKSTRUE):
 
         sockscheck(ip, SOCKSTRUE)
 
+def isSocks4(host, port, soc):
+    from struct import *
+    ipaddr = socket.inet_aton(host)
+    packet4 = "\x04\x01" + pack(">H",port) + ipaddr + "\x00"
+    soc.sendall(packet4)
+    data = soc.recv(8)
+    if(len(data)<2):
+        # Null response
+        return False
+    if data[0] != "\x00":
+        # Bad data
+        return False
+    if data[1] != "\x5A":
+        # Server returned an error
+        return False
+    return True
+
+def isSocks5(host, port, soc):
+    soc.sendall("\x05\x01\x00")
+    data = soc.recv(2)
+    if(len(data)<2):
+        # Null response
+        return False
+    if data[0] != "\x05":
+        # Not socks5
+        return False
+    if data[1] != "\x00":
+        # Requires authentication
+        return False
+    return True
+
+
 def sockscheck(ip, SOCKSTRUE):
     if SOCKSTRUE != 0:
-        ports = [1080,1075,10000,10080,10099,10130,10242,10777,1025,1026,1027,1028,1029,1030,1031,1032,1033,1039,1050,1066,1081,1098,11011,11022,11033,11055,11171,1122,11225,1180,1182,1200,1202,1212,1234,12654,1337,14841,16591,17327,1813,18888,1978,1979,19991,2000,21421,22277,2280,24971,24973,25552,25839,26905,28882,29992,3127,3128,32167,3330,3380,34610,3801,3867,40,4044,41080,41379,43073,43341,443,44548,4471,43371,44765,4914,49699,5353,559,58,6000,62385,63808,6551,6561,6664,6748,6969,7007,7080,8002,8009,8020,8080,8085,8111,8278,8751,8888,9090,9100,9988,9999,59175,5001,19794]
+        ports = [1080,1075,10000,10080,10099,10130,10242,10777,1025,1026,1027,1028,1029,1030,1031,1032,1033,1039,1050,1066,1081,1098,11011,11022,11033,11055,11171,1122,11225,1180,1182,1200,1202,1212,1234,12654,1337,14841,16591,17327,1813,18888,1978,1979,19991,2000,21421,22277,2280,24971,24973,25552,25839,26905,28882,29992,3127,3128,32167,3330,3380,34610,3801,3867,40,4044,41080,41379,43073,43341,443,44548,4471,43371,44765,4914,49699,5353,559,58,6000,62385,63808,6551,6561,6664,6748,6969,7007,7080,8002,8009,8020,8080,8085,8111,8278,8751,8888,9090,9100,9988,9999,59175,5001,19794,36510]
 
-        def check_socks(ip, port):
-            print "[SCANNING]: SOCKS testing " + str(port)
+        def getSocksVersion(proxy, port):
+            newprox = str(proxy) + ":" + str(port)
+            host = proxy
+            sch = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sch.settimeout(2)
             try:
-                sen = struct.pack('BBB', 0x05, 0x01, 0x00)
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(2)
-                s.connect(( ip , int( port )  ))
-                s.sendall(sen)
-                data = s.recv(2)
-                s.close()
-                version, auth = struct.unpack('BB', data)
-                print 'server : port  is  ', ip, ':', port, '; varsion: ', version
-            except Exception as e:
-                print e
-                #print "Port " + str(port) + " is dead"
-                pass
+                sch.connect((host, port))
+                if(isSocks4(host, port, sch)):
+                    sch.close()
+                    #print "DETECTED SOCKS5 " + str(newprox)
+                    return
+                elif(isSocks5(host, port, sch)):
+                    sch.close()
+                    #print "DETECTED SOCKS4 " + str(newprox)
+                    return
+                else:
+                    #("Not a SOCKS: " + newprox)
+                    sch.close()
+                    return 0
+            except socket.timeout:
+                #print "Timeout: " + newprox
+                sch.close()
+                return 0
+            except socket.error:
+                #print "Connection refused: " + newprox
+                sch.close()
+                return 0
 
         for newport in ports:
-            socksc = Process(target=check_socks, args=(ip, newport))
+            socksc = Process(target=getSocksVersion, args=(ip, newport))
             socksc.start()
 
         return
